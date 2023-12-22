cae9178acdb4a5a92ab01b03910608a2de762583
ryanfavour4 <ryanfave022@gmail.com>
2023-12-22 09:53:14 +0100

    navbar created and responsive -fixing-nav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
+  {
+    path: '**',
+    component: NotfoundComponent,
+  },
 ];
