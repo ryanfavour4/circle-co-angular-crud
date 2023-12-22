d09105267c37a1801f2fc0995884b3a57188a907
ryanfavour4 <ryanfave022@gmail.com>
2023-12-22 17:53:57 +0100

    HERO SECTION DONE

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../components/button/button.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  imports: [ButtonComponent, HeroComponent],
+  imports: [ButtonComponent, HeroComponent, NavbarComponent],
 })
 export class HomeComponent {
   iconFill = 'currentColor'; // Default to current text color
